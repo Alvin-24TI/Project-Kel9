9b6da940b508d0b9e61c6e6fb08aedf1f43fd2b0
Syauqi1TIE <syauqi24ti@mahasiswa.pcr.ac.id>
2026-05-12 12:44:09 +0700

    P8 - half

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,57 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import './css/style.css';
-import './charts/ChartjsConfig';
+import "./css/style.css";
+import "./charts/ChartjsConfig";
 
-// Import pages
-import Dashboard from './pages/Dashboard';
-import Analytics from './pages/Analytics';
-import Fintech from './pages/Fintech';
-import Customer from './pages/Customer';
-import Orders from './pages/Orders';
-import Invoices from './pages/Invoice';
-import Shop from './pages/Shop';
-import MyAccount from './pages/myAccount';
-import MyNotification from './pages/myNotification';
-import Feedback from './pages/Feedback';
-import Inbox from './pages/Inbox';
+import Layout from "./pages/Layout";
+import AuthLayout from "./layouts/AuthLayout";
+
+
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
+const Analytics = React.lazy(() => import("./pages/Analytics"));
+const Fintech = React.lazy(() => import("./pages/Fintech"));
+const Customer = React.lazy(() => import("./pages/Customer"));
+const Orders = React.lazy(() => import("./pages/Orders"));
+const Invoices = React.lazy(() => import("./pages/Invoice"));
+const Shop = React.lazy(() => import("./pages/Shop"));
+const MyAccount = React.lazy(() => import("./pages/myAccount"));
+const MyNotification = React.lazy(() => import("./pages/myNotification"));
+const Feedback = React.lazy(() => import("./pages/Feedback"));
+const Inbox = React.lazy(() => import("./pages/Inbox"));
+const Login = React.lazy(() => import("./pages/auth/Login"));
+const Register = React.lazy(() => import("./pages/auth/Register"));
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"));
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]);
 
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/analytics" element={<Analytics />} />
-      <Route path="/fintech" element={<Fintech />} />
-      <Route path="/customer" element={<Customer />} />
-      <Route path="/orders" element={<Orders />} />
-      <Route path="/invoices" element={<Invoices />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/my-account" element={<MyAccount />} />
-      <Route path="/my-notification" element={<MyNotification />} />
-      <Route path="/feedback" element={<Feedback />} />
-      <Route path="/inbox" element={<Inbox />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="fintech" element={<Fintech />} />
+        <Route path="customer" element={<Customer />} />
+        <Route path="orders" element={<Orders />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="my-account" element={<MyAccount />} />
+        <Route path="my-notification" element={<MyNotification />} />
+        <Route path="feedback" element={<Feedback />} />
+        <Route path="inbox" element={<Inbox />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="forgot" element={<Forgot />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Sidebar from '../partials/Sidebar';
-import Header from '../partials/Header';
 import FilterButton from '../components/DropdownFilter';
 import Datepicker from '../components/Datepicker';
 import DashboardCard01 from '../partials/dashboard/DashboardCard01';
@@ -20,23 +18,8 @@ import DashboardCard13 from '../partials/dashboard/DashboardCard13';
 import Banner from '../partials/Banner';
 
 function Dashboard() {
-
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
-    <div className="flex h-screen overflow-hidden">
-
-      {/* Sidebar */}
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-      {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-
-        {/*  Site header */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-        <main className="grow">
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+    <>
 
             {/* Dashboard actions */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
@@ -95,13 +78,8 @@ function Dashboard() {
               
             </div>
 
-          </div>
-        </main>
-
-        <Banner />
-
-      </div>
-    </div>
+      <Banner />
+    </>
   );
 }
 
