fbd965f8a31603d2092a7de66ee21b34dffe6ff3
Alvin-24TI <alvin24ti@mahasiswa.pcr.ac.id>
2026-06-17 05:37:42 +0700

    App icon

--- src/components/DropdownProfile.jsx
+++ src/components/DropdownProfile.jsx
@@ -43,7 +43,7 @@ function DropdownProfile({
         onClick={() => setDropdownOpen(!dropdownOpen)}
         aria-expanded={dropdownOpen}
       >
-        <img className="w-8 h-8 rounded-full" src={UserAvatar} width="32" height="32" alt="User" />
+        <img className="w-8 h-8 rounded-full" src="src\assets\Sobremesa.jpg" width="32" height="32" alt="User" />
         <div className="flex items-center truncate">
           <span className="truncate ml-2 text-sm font-medium text-gray-600 dark:text-gray-100 group-hover:text-gray-800 dark:group-hover:text-white">Sobremesa.</span>
           <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-500" viewBox="0 0 12 12">
